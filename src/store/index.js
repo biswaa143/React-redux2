a844f69b7336cb4737848fe12d88c1c1c66fbd48
Biswa <barikbiswa143@gmail.com>
2023-05-09 21:03:48 +0530

    Auth Reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 // import { createStore } from "redux";
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
-const counterSlice =  createSlice ({
-  name: 'counter',
-  initialState,
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -14,10 +14,25 @@ const counterSlice =  createSlice ({
       state.counter--;
     },
     increase(state, action) {
-      state.counter = state.counter + action.payload;
+      state.counter += action.payload;
     },
     togglecounter(state) {
       state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
     },
   }
 });
@@ -51,22 +66,24 @@ const counterSlice =  createSlice ({
 //     }
 //   }
 
-  // if (action.type === "INCREMENTBY5") {
-  //   return {
-  //     counter: state.counter + 5,
-  //   };
-  // }
-  // if (action.type === "DECREMENTBY5") {
-  //   return {
-  //     counter: state.counter - 5,
-  //   };
-  // }
+// if (action.type === "INCREMENTBY5") {
+//   return {
+//     counter: state.counter + 5,
+//   };
+// }
+// if (action.type === "DECREMENTBY5") {
+//   return {
+//     counter: state.counter - 5,
+//   };
+// }
 //   return state;
 // };
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterSlice.reducer, auth:authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;
